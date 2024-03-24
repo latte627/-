59a54673645bd6821ac75e9450d7fa053bcecf9f
豆豆 <328620267@qq.com>
2024-03-24 15:01:22 +0800

    1.1 更新显示屏的滑动功能

--- Calculator/app/src/main/java/com/example/calculator/Number.java
+++ Calculator/app/src/main/java/com/example/calculator/Number.java
@@ -1,8 +1,20 @@
 package com.example.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Number extends AppCompatActivity {
 
@@ -10,5 +22,25 @@ public class Number extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.number);
+
+        HorizontalScrollView scrollView = findViewById(R.id.scroll_view);
+
+// 在布局完成后滚动到底部
+        scrollView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // 移除监听以避免重复调用
+                scrollView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                // 滚动到底部
+                scrollView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scrollView.fullScroll(ScrollView.FOCUS_RIGHT);
+                    }
+                });
+            }
+        });
+
     }
 }
